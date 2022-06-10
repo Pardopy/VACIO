962e33b8df773e885934db8a197d3a7b26206d27
Pardopy <81496076+Pardopy@users.noreply.github.com>
2022-06-10 17:52:03 -0600

    Add files via upload

---
+++ tarea1web/src/main/java/com/example/demo/Tarea1webApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1webApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1webApplication.class, args);
+	}
+
+}
